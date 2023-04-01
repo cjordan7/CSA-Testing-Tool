cd2aa667a5366a83a3482511a1a09d7177e9a44b
CosmeJordan <cosme.jordan@rigi.tech>
2023-04-01 21:05:47 +0200

    Updates

--- INSTALL_MAGMA.sh
+++ INSTALL_MAGMA.sh
@@ -16,9 +16,9 @@ bash workdir/magma/targets/libpng/build.sh
 
 mkdir -p workdir/magma_libs/libsndfile
 export TARGET=workdir/magma_libs/libsndfile
-bash workdir/magma/targets/libsndfile/preinstall.sh
+sudo bash workdir/magma/targets/libsndfile/preinstall.sh
 # Magma line is buggy
-apt-get update && \
+sudo apt-get update && \
     apt-get install -y git make autoconf autogen automake build-essential libasound2-dev \
   libflac-dev libogg-dev libtool libvorbis-dev libopus-dev libmp3lame-dev \
   libmpg123-dev pkg-config python-is-python3
@@ -28,42 +28,42 @@ bash workdir/magma/targets/libsndfile/build.sh
 
 mkdir -p workdir/magma_libs/libtiff
 export TARGET=$(pwd)/workdir/magma_libs/libtiff
-bash workdir/magma/targets/libtiff/preinstall.sh
+sudo bash workdir/magma/targets/libtiff/preinstall.sh
 bash workdir/magma/targets/libtiff/fetch.sh
 bash workdir/magma/targets/libtiff/build.sh
 
 mkdir -p workdir/magma_libs/libxml2
 export TARGET=$(pwd)/workdir/magma_libs/libxml2
-bash workdir/magma/targets/libxml2/preinstall.sh
+sudo bash workdir/magma/targets/libxml2/preinstall.sh
 bash workdir/magma/targets/libxml2/fetch.sh
 bash workdir/magma/targets/libxml2/build.sh
 
 mkdir -p workdir/magma_libs/lua
 export TARGET=$(pwd)/workdir/magma_libs/lua
-bash workdir/magma/targets/lua/preinstall.sh
+sudo bash workdir/magma/targets/lua/preinstall.sh
 bash workdir/magma/targets/lua/fetch.sh
 bash workdir/magma/targets/lua/build.sh
 
 mkdir -p workdir/magma_libs/openssl
 export TARGET=$(pwd)/workdir/magma_libs/openssl
-bash workdir/magma/targets/openssl/preinstall.sh
+sudo bash workdir/magma/targets/openssl/preinstall.sh
 bash workdir/magma/targets/openssl/fetch.sh
 bash workdir/magma/targets/openssl/build.sh
 
 mkdir -p workdir/magma_libs/php
 export TARGET=$(pwd)/workdir/magma_libs/php
-bash workdir/magma/targets/php/preinstall.sh
+sudo bash workdir/magma/targets/php/preinstall.sh
 bash workdir/magma/targets/php/fetch.sh
 bash workdir/magma/targets/php/build.sh
 
 mkdir -p workdir/magma_libs/poppler
 export TARGET=$(pwd)/workdir/magma_libs/poppler
-bash workdir/magma/targets/poppler/preinstall.sh
+sudo bash workdir/magma/targets/poppler/preinstall.sh
 bash workdir/magma/targets/poppler/fetch.sh
 bash workdir/magma/targets/poppler/build.sh
 
 mkdir -p workdir/magma_libs/sqlite3
 export TARGET=$(pwd)/workdir/magma_libs/sqlite3
-bash workdir/magma/targets/sqlite3/preinstall.sh
+sudo bash workdir/magma/targets/sqlite3/preinstall.sh
 bash workdir/magma/targets/sqlite3/fetch.sh
 bash workdir/magma/targets/sqlite3/build.sh
